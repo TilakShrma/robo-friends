c3ac6f4a8f4c6e99a37cd7ae077507bf9413580e
Tilak Sharma <tilak.sharma.mail@gmail.com>
2018-04-18 17:10:14 +0530

    using RestApi for displaying robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			robots : robots,
+			robots : [],
 			searchField : ''
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+			return response.json();
+		}).then(users => {
+			this.setState({robots : users});
+		})
 	}
 
 	onSearchChange = (event) => {
@@ -25,7 +33,9 @@ class App extends React.Component{
 			<div className="tc">
 				<h1 className="f1" >RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
 			</div>
 		);
 	}
